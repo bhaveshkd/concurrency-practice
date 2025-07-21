86ea2b005c6c07d17d5a9b717a4dabdc73fe4ba9
Bhavesh Kumar Daryani <bhavesh.k.daryani@gmail.com>
2025-07-21 15:12:42 +0530

    a basic example of Runnable interface class

---
+++ src/main/java/com/example/RunnableInterfaceExample.java
@@ -0,0 +1,38 @@
+package main.java.com.example;
+
+
+public class RunnableInterfaceExample {
+    public static void main(String[] args) {
+        Thread one = new Thread(new ThreadOne());
+        Thread two = new Thread(new ThreadTwo());
+
+        Thread three = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Thread Three: " + i);
+            }
+        });
+
+
+        one.start();
+        two.start();
+        three.start();
+    }
+}
+
+class ThreadOne implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Thread One: " + i);
+        }
+    }
+}
+
+class ThreadTwo implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Thread Two: " + i);
+        }
+    }
+}
