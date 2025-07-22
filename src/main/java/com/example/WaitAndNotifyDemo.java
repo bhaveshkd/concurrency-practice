3c91457ad1c3f8cf2ad9867ea356ac00a9c3483c
Bhavesh Kumar Daryani <bhavesh.k.daryani@gmail.com>
2025-07-22 12:46:12 +0530

    Basic wait and notify example

---
+++ src/main/java/com/example/WaitAndNotifyDemo.java
@@ -0,0 +1,50 @@
+package main.java.com.example;
+
+public class WaitAndNotifyDemo {
+    public static final Object LOCK = new Object();
+
+    public static void main(String[] args) {
+        Thread one  = new Thread(() -> {
+            try {
+                one();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread two  = new Thread(() -> {
+            try {
+                two();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        one.start();
+        two.start();
+
+        try {
+            one.join();
+            two.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void one() throws InterruptedException {
+        synchronized (LOCK){
+            System.out.println("Hello from method one...");
+            LOCK.wait();
+            System.out.println("Back again in method one!");
+        }
+    }
+
+    private static void two() throws InterruptedException {
+        synchronized (LOCK){
+            System.out.println("Hello from method two...");
+            LOCK.notify();
+            System.out.println("Hello from method two after notifying!");
+        }
+    }
+}
